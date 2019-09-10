94dd3e2157818d453eb0ddd7b31bc1f42286ba30
Jacob Nesbitt <jjnesbitt2@gmail.com>
2019-09-10 11:55:21 -0400

    Add shift cipher

---
+++ a1/shift.py
@@ -0,0 +1,41 @@
+import sys
+
+
+KEY = 3
+ASCII_BOTTOM = 65
+CHAR_SPACE = 26
+ENCRYPT_ARG = "-e"
+DECRYPT_ARG = "-d"
+
+
+def encrypt(msg):
+    msg = "".join(filter(lambda c: c.isalpha(), msg))
+    newMsg = "".join(
+        map(
+            lambda x: chr(((ord(x) + KEY - ASCII_BOTTOM) % CHAR_SPACE) + ASCII_BOTTOM),
+            msg.upper(),
+        )
+    )
+    return newMsg
+
+
+def decrypt(msg):
+    newMsg = "".join(
+        map(
+            lambda x: chr(((ord(x) - KEY - ASCII_BOTTOM) % CHAR_SPACE) + ASCII_BOTTOM),
+            msg.upper(),
+        )
+    )
+    return newMsg
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if DECRYPT_ARG in sys.argv:
+            index = sys.argv.index(DECRYPT_ARG)
+            print(decrypt(sys.argv[index + 1]))
+
+        if ENCRYPT_ARG in sys.argv:
+            index = sys.argv.index(ENCRYPT_ARG)
+            print(sys.argv)
+            print(encrypt(sys.argv[index + 1]))
